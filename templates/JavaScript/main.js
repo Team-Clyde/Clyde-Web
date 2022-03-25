cf2f5c4a80eb7e09c1ad7a6788a10f57ea7e7132
Srijan Samridh <srijansinha3@gmail.com>
2022-03-25 19:41:13 +0530

    added js file

---
+++ templates/JavaScript/main.js
@@ -0,0 +1,27 @@
+const texts = ["Welcome To Your TrusteeShip!"]
+let count=0;
+let index=0;
+let currenttext="";
+let letter="";
+
+(function type(){
+
+    if (count === texts.length){
+        count=0;
+    }
+    currenttext=texts[count]
+    letter = currenttext.slice(0,++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currenttext.length){
+
+        index=0;
+    }
+    setTimeout(type,250);
+})();
+
+window.onload= function (){
+     document.getElementById("transparent-container").style.color="white";
+     document.getElementById("transparent-container").style.transform="scale(1.1)";
+     document.getElementById("transparent-container").style.transition="2s";
+}
